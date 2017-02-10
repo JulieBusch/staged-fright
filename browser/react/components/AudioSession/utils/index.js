de283350644ca0e224797f9eaf7281017038e40d
C.S. Jiang <christine.s.jiang@gmail.com>
2017-02-10 15:42:01 -0500

    moved utils from invis Watson to audiosession very messily

--- browser/react/components/AudioSession/utils/index.js
+++ browser/react/components/AudioSession/utils/index.js
@@ -101,4 +101,158 @@ SoundMeter.prototype.stop = function() {
 		}
 	};
 
+//WATSON FUNCTIONS
+
+import recognizeMicrophone from 'watson-speech/speech-to-text/recognize-microphone';
+
+export const resetWatson = () => {
+    if (this.state.audioSource) {
+      this.stopTranscription();
+    }
+    this.setState({ rawMessages: [], formattedMessages: [] });
+}
+
+export const stopTranscription = () => {
+    //this.stream && this.stream.stop();
+    console.log(this.stream);
+    this.setState({ audioSource: null });
+}
+
+export const getRecognizeOptions = (extra) => {
+    var keywords = this.getKeywordsArr();
+    return Object.assign({
+      mediaStream: this.stream,
+      token: this.state.token, smart_formatting: true, // formats phone numbers, currency, etc. (server-side)
+      format: true, // adds capitals, periods, and a few other things (client-side)
+      model: this.state.model,
+      objectMode: true,
+      interim_results: true,
+      continuous: true,
+      word_alternatives_threshold: 0.01, // note: in normal usage, you'd probably set this a bit higher
+      keywords: keywords,
+      keywords_threshold: keywords.length
+        ? 0.01
+        : undefined, // note: in normal usage, you'd probably set this a bit higher
+      timestamps: true, // set timestamps for each word - automatically turned on by speaker_labels
+      speaker_labels: this.state.speakerLabels, // includes the speaker_labels in separate objects unless resultsBySpeaker is enabled
+      resultsBySpeaker: this.state.speakerLabels, // combines speaker_labels and results together into single objects, making for easier transcript outputting
+      speakerlessInterim: this.state.speakerLabels // allow interim results through before the speaker has been determined
+    }, extra);
+  }
+
+export const streamToWatson = () => {
+	this.setState({ audioSource: 'mic' });
+      // The recognizeMicrophone() method is a helper method provided by the watson-speech package
+      // It sets up the microphone, converts and downsamples the audio, and then transcribes it over a WebSocket connection
+      // It also provides a number of optional features, some of which are enabled by default:
+      //  * enables object mode by default (options.objectMode)
+      //  * formats results (Capitals, periods, etc.) (options.format)
+      //  * outputs the text to a DOM element - not used in this demo because it doesn't play nice with react (options.outputElement)
+      //  * a few other things for backwards compatibility and sane defaults
+      // In addition to this, it passes other service-level options along to the RecognizeStream that manages the actual WebSocket connection.
+      this.handleStream(recognizeMicrophone(this.getRecognizeOptions()));
+}
+
+export const handleStream = (stream) => {
+	    // cleanup old stream if appropriate
+    // if (this.stream) {
+    //   this.stream.stop();
+    //   this.stream.removeAllListeners();
+    //   this.stream.recognizeStream.removeAllListeners();
+    // }
+    this.stream = stream;
+    this.captureSettings();
+
+    // grab the formatted messages and also handle errors and such
+    stream.on('data', this.handleFormattedMessage).on('end', this.handleTranscriptEnd);  //possibly eventually implement error handling for w/e 
+
+    // when errors occur, the end event may not propagate through the helper streams.
+    // However, the recognizeStream should always fire a end and close events
+    stream.recognizeStream.on('end', () => {
+      if (this.state.error) {
+        this.handleTranscriptEnd();
+      }
+    });
+
+    // grab raw messages from the debugging events for display on the JSON tab
+    stream.recognizeStream
+      .on('message', (frame, json) => this.handleRawdMessage({sent: false, frame, json}))
+      .on('send-json', json => this.handleRawdMessage({sent: true, json}))
+      .once('send-data', () => this.handleRawdMessage({
+        sent: true, binary: true, data: true // discard the binary data to avoid wasting memory
+      }))
+      .on('close', (code, message) => this.handleRawdMessage({close: true, code, message}));
+
+    // ['open','close','finish','end','error', 'pipe'].forEach(e => {
+    //     stream.recognizeStream.on(e, console.log.bind(console, 'rs event: ', e));
+    //     stream.on(e, console.log.bind(console, 'stream event: ', e));
+    // });
+}
+
+export const handleRawdMessage = (msg) => {
+    this.setState({rawMessages: this.state.rawMessages.concat(msg)});
+}
+
+export const handleFormattedMessage = (msg) => {
+    this.setState({formattedMessages: this.state.formattedMessages.concat(msg)});
+}
+
+export const handleTranscriptEnd = () => { //connected to our shit 
+    // note: this function will be called twice on a clean end,
+    // but may only be called once in the event of an error
+    this.setState({audioSource: null}); 
+}
+
+export const watsonTokenSetup = () => {
+	this.fetchToken();
+    // tokens expire after 60 minutes, so automatcally fetch a new one ever 50 minutes
+    // Not sure if this will work properly if a computer goes to sleep for > 50 minutes and then wakes back up
+    // react automatically binds the call to this
+
+    this.setState({'tokenInterval' : setInterval(this.fetchToken, 50 * 60 * 1000) });
+}
+
+export const fetchToken = () => {
+    return fetch('/api/token').then(res => {
+      if (res.status != 200) {
+        throw new Error('Error retrieving auth token');
+      } 
+      return res.text();
+    }). // todo: throw here if non-200 status
+    then(token => this.setState({ token })).catch(console.log);
+}
+
+export const handleKeywordsChange = (e) => {
+    this.setState({ keywords: e.target.value });
+}
+
+export const getKeywordsArr = () => {
+    return this.state.keywords.split(',').map(k => k.trim()).filter(k => k);
+}
+
+export const getFinalResults = () => {
+  console.log(this.state.formattedMessages.filter(r => r.results && r.results.length && r.results[0].final));
+  return this.state.formattedMessages.filter(r => r.results && r.results.length && r.results[0].final);
+}
+
+export const getCurrentInterimResult = () => {
+  const r = this.state.formattedMessages[this.state.formattedMessages.length - 1];
+
+  // When resultsBySpeaker is enabled, each msg.results array may contain multiple results. However, all results
+  // in a given message will be either final or interim, so just checking the first one still works here.
+  if (!r || !r.results || !r.results.length || r.results[0].final) {
+    return null;
+  }
+  return r;
+}
+
+export const getFinalAndLatestInterimResult = () => {
+  const final = this.getFinalResults();
+  const interim = this.getCurrentInterimResult();
+  if (interim) {
+    final.push(interim);
+  }
+  return final;
+}
+
 export { findFundamentalFreq, freqToMIDI, stdSemitones, SoundMeter };
