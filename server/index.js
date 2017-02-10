93d8f567861e424d6d3fcf72356f1f29f58f2cdd
C.S. Jiang <christine.s.jiang@gmail.com>
2017-02-10 15:43:48 -0500

    token serving in server

--- server/index.js
+++ server/index.js
@@ -4,8 +4,31 @@ const express = require('express');
 const chalk = require('chalk');
 const configServer = require('./configure');
 const app = express();
+
+const SpeechToTextV1 = require('watson-developer-cloud/speech-to-text/v1');
+const AuthorizationV1 = require('watson-developer-cloud/authorization/v1');
+
 configServer(app);
 
+const stt = new SpeechToTextV1({
+  // if left undefined, username and password to fall back to the SPEECH_TO_TEXT_USERNAME and
+  // SPEECH_TO_TEXT_PASSWORD environment properties, and then to VCAP_SERVICES (on Bluemix)
+  username: '',
+  password: ''
+});
+
+const authService = new AuthorizationV1(stt.getCredentials());
+
+// Get token using your credentials
+app.get('/api/token', function(req, res, next) {
+  authService.getToken(function(err, token) {
+    if (err) {
+      next(err);
+    } else {
+      res.send(token);
+    }
+  });
+});
 
 app.get('/*', (req, res) => {
   res.sendFile(app.getValue('indexPath'));
