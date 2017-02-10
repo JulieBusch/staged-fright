b0249c5b7a3d5f872d4c318ccfb0c211a3847c39
C.S. Jiang <christine.s.jiang@gmail.com>
2017-02-10 15:41:01 -0500

    long-awaited spelling fix

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 'use strict';
 
-
-
 const app = require('./server');
 const chalk = require('chalk');
 
 const _Port = 3001;
 
-app.listen(_Port, () => console.log(chalk.magenta(`Staged Fright in now runnig on port ${_Port}`)));
+app.listen(_Port, () => console.log(chalk.magenta(`StagedFright is now running on port ${_Port}`)));
