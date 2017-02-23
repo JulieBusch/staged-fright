3ef103c2052f0c50178e0e73a2c54cd847ee1c59
C.S. Jiang <christine.s.jiang@gmail.com>
2017-02-22 21:08:06 -0500

    refactored PitchTracker for testing

--- src/browser/react/components/PitchTracker/index.js
+++ src/browser/react/components/PitchTracker/index.js
@@ -1,22 +1,43 @@
 import React from 'react';
 
-export default (props) => (
-      <a-entity>
-        <a-entity
-          material={`color: ${colorChange(props.pitch)}`}
-          position="5.00 6 -10.00"
-          rotation="0 7.42 0"
-          geometry="primitive: circle; radius: 0.5">
-        </a-entity>
-        <a-entity position="8.80 6 -8.77"
-                  scale="10 10 10"
-                  text="value: P\nI\nT\nC\nH; line-height: 30px;">
-        </a-entity>
-      </a-entity>
-    )
-
-function colorChange(pitch) {
-  return pitch 
+export const colorChange = pitch => 
+  pitch 
   ? `#FFFF00`
   : `#00FF00`
-}
+
+export const PitchCircle = ({ pitch, position, rotation, geometry }) => (
+  <a-entity 
+    material={`color: ${colorChange(pitch)}`}
+    position={position}
+    rotation={rotation}
+    geometry={geometry}
+  >
+  </a-entity>
+);
+
+PitchCircle.defaultProps = {
+  position: "5.00 6 -10.00",
+  rotation: "0 7.42 0",
+  geometry: "primitive: circle; radius: 0.5",
+};
+
+export const PitchLabel = ({ position, scale, text }) => (
+  <a-entity position={position}
+            scale={scale}
+            text={text}>
+  </a-entity>
+);
+
+PitchLabel.defaultProps = {
+  position: "8.80 6 -8.77",
+  scale: "10 10 10", 
+  text: "value: P\nI\nT\nC\nH; line-height: 30px;",
+};
+
+export default (props) => (
+      <a-entity>
+        <PitchCircle pitch={props.pitch}/>
+        <PitchLabel/>
+      </a-entity>
+);
+
